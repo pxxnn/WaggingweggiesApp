ce4b95c1636de7381c03d757ebe8db91552e31f0
piix <pattarawadee.chuen@gmail.com>
2024-03-28 15:14:36 +0700

    Update

--- components/homepage/ServiceFlatList.js
+++ components/homepage/ServiceFlatList.js
@@ -6,18 +6,12 @@ import { Image } from "react-native";
 export default function ServiceFlatList(props) {
 
     
-    const tours = [
-        { "id": "1", "title": "Tour in London", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-1.jpg" },
-        { "id": "2", "title": "Tour in Paris", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-2.jpg" },
-        { "id": "3", "title": "Tour in Italy", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-3.jpg" },
-        { "id": "4", "title": "Tour in Portugal", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-4.jpg" },
-        { "id": "5", "title": "Tour in Netherlands", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-5.jpg" }
-    ];
+    
 
     const [onlineTours, setOnlineTours] = useState([]);
     const loadOnlineTours = async () => {
         try {
-            let promise = await fetch('https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/json/trips.json');
+            let promise = await fetch('https://raw.githubusercontent.com/pxxnn/WaggingweggiesApp/main/data.json?token=GHSAT0AAAAAACQHI4ZB2FEUVWQ3DXJNQL36ZQFE3SA');
             let data = await promise.json();
             console.log("Load Data : ", data);
             //SET STATE

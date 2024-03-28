ab6f3d968e875ebcc36e1ac9e8bbcf0a334da275
piix <pattarawadee.chuen@gmail.com>
2024-03-28 15:54:13 +0700

    Update

--- components/homepage/ServiceFlatList.js
+++ components/homepage/ServiceFlatList.js
@@ -1,20 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, ScrollView, View } from "react-native";
-import { Text } from "react-native";
-import { Image } from "react-native";
+import { FlatList, View, Text, Image, StyleSheet } from "react-native";
 
 export default function ServiceFlatList(props) {
-
-    
-    
 
     const [onlineTours, setOnlineTours] = useState([]);
     const loadOnlineTours = async () => {
         try {
-            let promise = await fetch('https://raw.githubusercontent.com/pxxnn/WaggingweggiesApp/main/data.json?token=GHSAT0AAAAAACQHI4ZB2FEUVWQ3DXJNQL36ZQFE3SA');
+            let promise = await fetch('https://raw.githubusercontent.com/pxxnn/WaggingweggiesApp/main/data.json');
             let data = await promise.json();
             console.log("Load Data : ", data);
-            //SET STATE
             setOnlineTours(data);
         } catch (error) {
             console.log("ERROR : ", error);
@@ -25,57 +19,50 @@ export default function ServiceFlatList(props) {
         loadOnlineTours();
     }, []);
 
-
-
     return (
         <View style={props.style}>
             <Text style={{ fontSize: 25 }}>Service</Text>
-            <Text style={{ fontSize: 15, color: 'gray', marginVertical: 10 }}>Let find out what most interesting things</Text>
-            <FlatList horizontal={true}
-                // data={tours}
+            <Text style={{ fontSize: 15, color: 'gray', marginVertical: 10 }}>Let's find out what's most interesting!</Text>
+            <FlatList 
+                horizontal={true}
                 data={onlineTours}
-                renderItem={
-                    ({ item, index }) => {
-                        console.log(item, index, item.uri);
-                        return (
-                            <View style={{ marginRight: 10 }}>
-                                <Image style={{ width: 200, height: 200, borderRadius: 10 }} source={{ uri: item.uri }} />
-                                <View style={{ marginTop: -50, height: 50, width: 200, paddingHorizontal: 10, backgroundColor: 'black', opacity: 0.5, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}>
-                                    <Text style={{ fontSize: 20, color: 'white' }}>{item.title}</Text>
-                                </View>
-                            </View>
-                        );
-                    }
-                }
+                renderItem={({ item }) => (
+                    <View style={styles.itemContainer}>
+                        <Image style={styles.image} source={{ uri: item.uri }} />
+                        <View style={styles.imageOverlay}>
+                            <Text style={styles.title}>{item.title}</Text>
+                        </View>
+                    </View>
+                )}
                 keyExtractor={item => item.id}
-            >
-                <View style={{ flexDirection: "row" }}>
-                    {/* <TourItem />
-                    <TourItem />
-                    <TourItem /> */}
-                    <View style={{ marginRight: 10 }}>
-                        <Image style={{ width: 200, height: 200, borderRadius: 10 }} source={{ uri: 'https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-1.jpg' }} />
-                        <View style={{ marginTop: -50, height: 50, width: 200, paddingHorizontal: 10, backgroundColor: 'black', opacity: 0.5, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}>
-                            <Text style={{ fontSize: 20, color: 'white' }}>Tour in Somewhere</Text>
-                        </View>
-                    </View>
-
-                    <View style={{ marginRight: 10 }}>
-                        <Image style={{ width: 200, height: 200, borderRadius: 10 }} source={{ uri: 'https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-1.jpg' }} />
-                        <View style={{ marginTop: -50, height: 50, width: 200, paddingHorizontal: 10, backgroundColor: 'black', opacity: 0.5, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}>
-                            <Text style={{ fontSize: 20, color: 'white' }}>Tour in Somewhere</Text>
-                        </View>
-                    </View>
-
-                    <View style={{ marginRight: 10 }}>
-                        <Image style={{ width: 200, height: 200, borderRadius: 10 }} source={{ uri: 'https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/trip-1.jpg' }} />
-                        <View style={{ marginTop: -50, height: 50, width: 200, paddingHorizontal: 10, backgroundColor: 'black', opacity: 0.5, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}>
-                            <Text style={{ fontSize: 20, color: 'white' }}>Tour in Somewhere</Text>
-                        </View>
-                    </View>
-                </View>
-            </FlatList>
-
+            />
         </View>
     );
-} 
+}
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        marginRight: 10,
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+    },
+    imageOverlay: {
+        marginTop: -50,
+        height: 50,
+        width: 200,
+        paddingHorizontal: 10,
+        backgroundColor: 'black',
+        opacity: 0.5,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center',
+    },
+});
